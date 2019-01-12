512c7dee5e664d2b04906a660c515c5cc20ef913
songzhiyang <406469533@qq.com>
2019-01-12 09:32:11 +0800

    [feature]:第九章 启动插件activity PluginA添加activity

---
+++ plugina/src/main/java/com/szy/plugina/PluginAActivityA.java
@@ -0,0 +1,20 @@
+package com.szy.plugina;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by songzhiyang on 2019/1/12.
+ *
+ * @author songzhiyang
+ */
+public class PluginAActivityA extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_plugin_a_a);
+    }
+}
